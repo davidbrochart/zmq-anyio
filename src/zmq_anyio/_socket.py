1bb478c0339ac9fe356ba44a23e71232e7e530f1
David Brochart <david.brochart@gmail.com>
2024-11-07 12:17:41 +0100

    Fix tests

--- src/zmq_anyio/_socket.py
+++ src/zmq_anyio/_socket.py
@@ -35,7 +35,7 @@ class _FutureEvent(NamedTuple):
 class _AsyncPoller(zmq.Poller):
     """Poller that returns a Future on poll, instead of blocking."""
 
-    _socket_class: type[_AsyncSocket]
+    _socket_class: type[Socket]
     raw_sockets: list[Any]
 
     def _watch_raw_socket(self, socket: Any, evt: int, f: Callable) -> None:
@@ -71,7 +71,7 @@ class _AsyncPoller(zmq.Poller):
 
             watcher.add_done_callback(lambda f: self._unwatch_raw_sockets(*raw_sockets))
 
-            wrapped_sockets: list[_AsyncSocket] = []
+            wrapped_sockets: list[Socket] = []
 
             def _clear_wrapper_io(f):
                 for s in wrapped_sockets:
@@ -81,7 +81,7 @@ class _AsyncPoller(zmq.Poller):
                 if isinstance(socket, zmq.Socket):
                     if not isinstance(socket, self._socket_class):
                         # it's a blocking zmq.Socket, wrap it in async
-                        socket = self._socket_class.from_socket(socket)
+                        socket = self._socket_class(socket)
                         wrapped_sockets.append(socket)
                     if mask & zmq.POLLIN:
                         create_task(
@@ -190,6 +190,7 @@ class Socket(zmq.Socket):
         self._fd = self._shadow_sock.FD
 
     def close(self, linger: int | None = None) -> None:
+        assert self._selector is not None
         self._selector.unregister(self._shadow_sock)
         if not self.closed and self._fd is not None:
             event_list: list[_FutureEvent] = list(
@@ -265,7 +266,7 @@ class Socket(zmq.Socket):
         obj: Any,
         flags: int = 0,
         **kwargs,
-    ) -> None:
+    ):
         send_kwargs = {}
         for key in ("routing_id", "group"):
             if key in kwargs:
@@ -275,7 +276,7 @@ class Socket(zmq.Socket):
 
     async def asend_multipart(
         self,
-        msg_parts: Sequence[bytes],
+        msg_parts: list[bytes],
         flags: int = 0,
         copy: bool = True,
         track: bool = False,
@@ -667,6 +668,8 @@ class Socket(zmq.Socket):
             self._task_group = await exit_stack.enter_async_context(create_task_group())
             self._exit_stack = exit_stack.pop_all()
             self._task_group.start_soon(self.start)
+
+        return self
 
     async def __aexit__(self, exc_type, exc_value, exc_tb):
         self._task_group.cancel_scope.cancel()
